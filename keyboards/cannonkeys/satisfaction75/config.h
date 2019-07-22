80afd02cb2ae4120ecd1de6b318a776074385bbb
DarkMio <miostarkid@gmail.com>
2019-07-22 22:40:41 +0200

    change pin assignments to get rid of disconnected development pins

--- keyboards/cannonkeys/satisfaction75/config.h
+++ keyboards/cannonkeys/satisfaction75/config.h
@@ -32,7 +32,7 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #define MATRIX_COLS 16
 
 #define MATRIX_COL_PINS { B1, B2, B10, B11, B12, B13, B14, A8, A9, A10, B0, A7, A5, B5, A15, A1 }
-#define MATRIX_ROW_PINS { B3, B4, A0, A2, A4, A3 }
+#define MATRIX_ROW_PINS { B3, B4, A0, C3, A4, A3 }
 #define DIODE_DIRECTION COL2ROW
 
 #define ENCODERS_PAD_A { B9 }
